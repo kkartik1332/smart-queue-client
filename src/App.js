d5a1dedeaacccf2e7c1826e948630784e2391528
kkartik1332 <kkartik1332@gmail.com>
2026-05-01 20:46:38 +0530

    beep sound add

--- src/App.js
+++ src/App.js
@@ -129,8 +129,25 @@ function MainApp() {
       setLoading(false);
     }
   };
+  const playBeep = () => {
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const oscillator = audioCtx.createOscillator();
+  const gainNode = audioCtx.createGain();
+
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+
+  oscillator.type = 'sine';
+  oscillator.frequency.value = 880;
+  gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
+  gainNode.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 0.5);
+
+  oscillator.start(audioCtx.currentTime);
+  oscillator.stop(audioCtx.currentTime + 0.5);
+};
 
   const callNext = async (svc) => {
+    playBeep();
     try {
       await axios.post(`${API}/next`, { service: svc });
       fetchQueue();
